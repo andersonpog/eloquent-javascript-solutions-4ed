8590b59558ae4a834b26b32770bfe8ad2db026c6
Anderson Silveira <silveira.anderson.1988@gmail.com>
2025-01-15 15:41:50 -0300

    Update 01 Looping a triangle.js

--- chapter 02/01 Looping a triangle.js
+++ chapter 02/01 Looping a triangle.js
@@ -1,10 +1,8 @@
 // Your code here.
-for(let i=1; i<=7; i++)
-  {
-    let s = ""
-    for(let j=0; j<i;j++)
-      {
-        s += "#"
-      }
-    console.log(s)
+for(let i=1; i<=7; i++){
+  let s = ""
+  for(let j=0; j<i;j++){
+    s += "#"
   }
+  console.log(s)
+}
