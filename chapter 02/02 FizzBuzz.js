a444c87eb86782afec5cdd21aba13a40d8e238b2
Anderson Silveira <silveira.anderson.1988@gmail.com>
2025-01-15 15:52:42 -0300

    Update 02 FizzBuzz.js

--- chapter 02/02 FizzBuzz.js
+++ chapter 02/02 FizzBuzz.js
@@ -8,3 +8,13 @@ for(let i=1; i<=100; i++){
     console.log("Buzz")
   else console.log(i)
 }
+
+// Clever
+for(let i=1; i<=100; i++){
+  let s = ''
+  if(i%3==0)
+    s+="Fizz"
+  if(i%5==0)
+    s+="Buzz"
+  console.log(s||i)
+}
