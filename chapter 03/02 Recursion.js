2e86dd73dbc2d7322cf2c94f6b60900e4fca41e7
Anderson Silveira <silveira.anderson.1988@gmail.com>
2025-01-21 16:40:00 -0300

    Create 02 Recursion.js

---
+++ chapter 03/02 Recursion.js
@@ -0,0 +1,13 @@
+// Your code here.
+function isEven(num){
+  if(num == 0) return true
+  if(num == 1) return false
+  return isEven(num-2)
+}
+
+console.log(isEven(50));
+// → true
+console.log(isEven(75));
+// → false
+console.log(isEven(-1));
+// → ??
